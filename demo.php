4d7594b03fc2f3351e744058e57f84b6a13c2ff0
Azhari Harahap <blackarazi@gmail.com>
2012-10-20 23:16:46 +0700

    update demo server link

--- demo.php
+++ demo.php
@@ -7,16 +7,15 @@
 		<h3>Demo</h3>
 		<p>To see how is Kalkun looks like in real system, you can try it on our demo server. There are 3 version of the demo: </p>
 		<ul>
-			<li><a href="http://203.34.118.83/kalkun/demo/release/0.5">Latest released version (0.5)</a></li>
-			<li><a href="http://203.34.118.83/kalkun/demo/development/master">Latest development version (master branch)</a></li>
-			<li><a href="http://203.34.118.83/kalkun/demo/development/devel">Latest development version (devel branch)</a></li>
+			<li><a href="http://199.175.55.123/kalkun/demo/release/0.5">Latest released version (0.5)</a></li>
+			<li><a href="http://199.175.55.123/kalkun/demo/development/master">Latest development version (master branch)</a></li>
+			<li><a href="http://199.175.55.123/kalkun/demo/development/devel">Latest development version (devel branch)</a></li>
 		</ul>
 						
 		<p>Then login with username: 'kalkun', password 'kalkun' (both without quotes).</p>
 		<p><strong>Please NOTE</strong> that you can do anything on the demo, but please <strong>DO NOT</strong> change the <i>kalkun</i> user password. All data will be cleanup every midday and midnight.</p>
 				
 		<p>Dummy data on this demo is generated with <a rel="nofollow" href="http://generatedata.com">generatedata.com</a>.</p>
-		<p>Demo server is provided by <a href="http://airputih.or.id">AirPutih</a>.</p>		
 		</div>
 		
 		<div id="sidebar">
